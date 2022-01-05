f0e8a6bab2bbb7ab4fb7db47d702dfa1bf2bb010
Alexander Isenko <alex.isenko@protonmail.com>
2022-01-05 17:39:01 +0100

    renamed main.qml to MainView.qml

--- trdrop/trdrop-ui/source/main.cpp
+++ trdrop/trdrop-ui/source/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/views/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/views/MainView.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject* obj, const QUrl& objUrl) {
             if (!obj && url == objUrl)
