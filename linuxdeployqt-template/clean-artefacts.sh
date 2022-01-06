0c0d901ad44a8e83cedfff88502c7ab9554971c3
Alexander Isenko <alex.isenko@protonmail.com>
2022-01-06 18:17:21 +0100

    Release - linux workflow debugging #23

---
+++ linuxdeployqt-template/clean-artefacts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm ./usr/bin/qt.conf ./usr/bin/trdrop-ui
+
+rm ./usr/lib/lib*
+rm -r ./usr/translations
+rm -r ./usr/share/doc
+rm -r ./usr/plugins
+
+rm ./AppRun ./.DirIcon ./trdrop.desktop ./trdrop_logo.png ./*.AppImage
